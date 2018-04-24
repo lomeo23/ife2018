9dc20b06e50a06e268d80e22399d65436198e15f
pikadun <wg0121@outlook.com>
2018-04-24 22:06:25 +0800

    Modify the syntax

--- Designer/task1/index.js
+++ Designer/task1/index.js
@@ -1,7 +1,7 @@
 let bool = true;
 let $hr = document.getElementById("hr");
 let $title = document.getElementById("title");
-document.getElementById("btn").addEventListener("click", function () {
+document.getElementById("btn").addEventListener("click", () => {
     if (bool) {
         $hr.style.width = "300px";
         $title.style.color = "#1749ee";
